a1cb7d484c149e30aef53a9b271c9e97d1b66840
ariduki <ariduki@gmail.com>
2023-08-09 22:48:50 +0800

    init server

--- server/main.go
+++ server/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/buy", handler)
+	r.Run("localhost:19810")
+}
+
+func handler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"result": "buy item",
+	})
+}
