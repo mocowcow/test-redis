cda5bb0cfd8080f6d426f6f2cb4b9500558bec70
ariduki <ariduki@gmail.com>
2023-08-10 22:08:16 +0800

    add access limit

--- server/main.go
+++ server/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis"
+)
+
+const (
+	ACCESS_LIMIT = 5
+	COOLDOWN     = 2
 )
 
 var RC *redis.Client = redis.NewClient(&redis.Options{
@@ -12,13 +19,28 @@ var RC *redis.Client = redis.NewClient(&redis.Options{
 })
 
 func main() {
+
+	RC.FlushDB()
+
 	r := gin.Default()
-	r.GET("/buy", handler)
+	r.GET("/buy", buy)
 	r.Run("localhost:19810")
 }
 
-func handler(c *gin.Context) {
+func buy(c *gin.Context) {
+	cIP := c.ClientIP()
+	freq, _ := RC.Incr(cIP).Result()
+	RC.Expire(cIP, COOLDOWN*time.Second)
+
+	if freq > ACCESS_LIMIT {
+		c.JSON(429, gin.H{
+			"result": "too many request",
+		})
+		return
+	}
+
 	c.JSON(200, gin.H{
 		"result": "buy item",
+		"freq":   freq,
 	})
 }
